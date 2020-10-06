145b1ab89bd167416dbfaf6550c48ed51f5a4306
MIchael Ekornrud <s341866@oslomet.no>
2020-10-06 11:28:51 +0200

    Oppgave 1: Fikset på antall() metoden

--- src/oblig2/DobbeltLenketListe.java
+++ src/oblig2/DobbeltLenketListe.java
@@ -77,9 +77,10 @@ public class DobbeltLenketListe<T> implements Liste<T>{
         if (tom()){
             return 0;
         }
+        antall = 0;
         Node current = hode;
 
-        while (current.neste != null){
+        while (current != null){
             current = current.neste; //oppdaterer det nåverende tallet.
             antall++; //oppdaterer antall
         }
@@ -856,37 +857,40 @@ public class DobbeltLenketListe<T> implements Liste<T>{
         }
 
         int curr_index = 0;
+        int next_index = 0;
         T curr_value = liste.hent(0);
         T tmp = curr_value;
-        for (int i = 0; i < liste.antall() - 1; ++i){
-            for (int j = 1; j < liste.antall();  ++j){
-                if (liste.hent(i) != null) {
-                    if (liste.hent(j) != null) {
-                        curr_index = i;
-                        curr_value = liste.hent(i);
-                        tmp = liste.hent(j);
+        Node<T> current;
+        Node<T> next;
+        for (int i = 0; i < liste.antall() -1; ++i){
+            for (int j = 1; j < liste.antall(); ++j) {
+                curr_index = i;
+                next_index = j;
+                System.out.println("Liste.antall() : " + liste.antall());
+                System.out.println("i: " + curr_index + " j: " + next_index);
+                if (liste.hent(i) != null && liste.hent(i + 1) != null) {
+                    curr_value = liste.hent(curr_index);
+                    tmp = liste.hent(next_index);
 
-                        //Tilfelle 1: Hvis verdiene er like --> Trenger ikke å sortere
-                        if (c.compare(curr_value, tmp) == 0) {
-                            return;
-                        }
-                        //Tilfelle 2: Hvis a er større enn b
-                        else if (c.compare(curr_value, tmp) > 0) {
-                            liste.oppdater(i, tmp);
-                            liste.oppdater(j, curr_value);
-                        }
-
-                        //Tilfelle 3: Hvis b er større enn a --> Trenger ikke å sortere
-                        else if (c.compare(curr_value, tmp) < 0) {
-                            return;
-
-                        } else {
-                            System.out.println("Du gjøre feil i den oppgave");
-                        }
-                        System.out.println("Liste: " + liste);
-
-
+                    //Tilfelle 1: Hvis verdiene er like --> Trenger ikke å sortere
+                    if (c.compare(curr_value, tmp) == 0) {
+                        return;
                     }
+                    //Tilfelle 2: Hvis a er større enn b
+                    else if (c.compare(curr_value, tmp) > 0) {
+                        liste.oppdater(curr_index, tmp);
+                        liste.oppdater(next_index, curr_value);
+                    }
+
+                    //Tilfelle 3: Hvis b er større enn a --> Trenger ikke å sortere
+                    else if (c.compare(curr_value, tmp) < 0) {
+                        return;
+
+                    } else {
+                        System.out.println("Du gjøre feil i den oppgave");
+                    }
+                    System.out.println("Liste: " + liste);
+
                 }
             }
         }
